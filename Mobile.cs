c8e96ed54a851d74843c079e3f7ff0d35c04c05e
Ekramuddin <mca20009@glbitm.ac.in>
2023-07-11 17:58:36 +0530

    Add project files.

---
+++ Mobile.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotNetPractice
+{
+    public class Mobile
+    {
+      public static void Main(string[] args)
+        {
+            Smartphone obj = new Smartphone();
+            obj.Name = "Apple";
+            obj.Price = 75999;
+            obj.colour = "white";
+            Console.WriteLine(obj.Name  +" "+ obj.colour +" "+ obj.Price);
+            
+        }
+    }
+}
---
+++ Smartphone.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotNetPractice
+{
+    public class Smartphone
+    {
+        public string Name;
+        public Double Price;
+        public String colour;
+    }
+}
+
